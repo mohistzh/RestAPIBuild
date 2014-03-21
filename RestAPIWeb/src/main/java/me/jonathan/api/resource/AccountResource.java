37eabf68fd33e0388121d5bb93090c77887f3b46
Jonathan Zhang <mohistzh@gmail.com>
2014-03-21 19:58:45 +0800

    update account api response type

--- RestAPIWeb/src/main/java/me/jonathan/api/resource/AccountResource.java
+++ RestAPIWeb/src/main/java/me/jonathan/api/resource/AccountResource.java
@@ -3,6 +3,8 @@ package me.jonathan.api.resource;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import me.jonathan.api.entity.Account;
 import me.jonathan.sp.AccountManagement;
@@ -26,6 +28,7 @@ public class AccountResource {
 	 */
 	@GET
 	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
 	public Account getAccountById(@PathParam("id") Integer id) {
 		AccountManagement am = new AccountManagementImpl();
 		me.jonathan.sp.bean.Account account = am.getAccountById(id);
